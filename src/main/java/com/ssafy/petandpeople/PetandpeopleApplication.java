94997b5d5722a91393df04cec42aef404f71d09c
kyoungbeom <pkb1998@naver.com>
2024-11-11 15:06:33 +0900

    initial setting

---
+++ src/main/java/com/ssafy/petandpeople/PetandpeopleApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.petandpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetandpeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetandpeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/petandpeople/PetandpeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.petandpeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetandpeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
