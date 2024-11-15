1aff738b98980d641bb7bb259c88ded3d72082d5
김윤 <yoon733@naver.com>
2024-11-16 00:52:06 +0900

    Type : style Subject : EmailDto getter, setter 위치 이동

--- src/main/java/com/ssafy/petandpeople/application/dto/EmailDto.java
+++ src/main/java/com/ssafy/petandpeople/application/dto/EmailDto.java
@@ -3,19 +3,18 @@ package com.ssafy.petandpeople.application.dto;
 public class EmailDto {
 
     private String email;
-
     private String authCode;
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getAuthCode() {
         return authCode;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public void setAuthCode(String authCode) {
