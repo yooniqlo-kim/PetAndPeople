c9f4f6e7cf3e81bdead5ef0b6b1fcc983ed1f42d
김윤 <yoon733@naver.com>
2024-11-17 01:49:14 +0900

    Type : feat Subject : userDto validation 관련 예외 처리 구현

--- src/main/java/com/ssafy/petandpeople/presentation/exceptionhandler/user/UserExceptionHandler.java
+++ src/main/java/com/ssafy/petandpeople/presentation/exceptionhandler/user/UserExceptionHandler.java
@@ -5,12 +5,18 @@ import com.ssafy.petandpeople.common.exception.user.UserNotFoundException;
 import com.ssafy.petandpeople.presentation.response.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class UserExceptionHandler {
+
     private static final Logger log = LoggerFactory.getLogger(UserExceptionHandler.class);
 
     @ExceptionHandler(value = UserNotFoundException.class)
@@ -22,6 +28,22 @@ public class UserExceptionHandler {
         return ResponseEntity
                 .status(errorCodeIfs.getHttpStatusCode())
                 .body(Api.ERROR(errorCodeIfs));
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Api<Object>> exceptionHandler(MethodArgumentNotValidException methodArgumentNotValidException) {
+        String errorMessage =
+                methodArgumentNotValidException.getBindingResult().getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(FieldError::getDefaultMessage)
+                .get();
+
+        log.error("MethodArgumentNotValidException occurred : {}", errorMessage);
+
+        return ResponseEntity
+                .status(400)
+                .body(Api.ERROR(1001, errorMessage));
     }
 
 }
