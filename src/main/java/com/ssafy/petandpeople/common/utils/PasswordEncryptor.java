3ad1e279470b05a81f0f58f03e779cd8c2fee847
김윤 <yoon733@naver.com>
2024-11-18 01:17:52 +0900

    Type : refactor Subject : PasswordEncryptor 코드 리팩토링

--- src/main/java/com/ssafy/petandpeople/common/utils/PasswordEncryptor.java
+++ src/main/java/com/ssafy/petandpeople/common/utils/PasswordEncryptor.java
@@ -1,14 +1,13 @@
 package com.ssafy.petandpeople.common.utils;
 
+import com.ssafy.petandpeople.common.exception.user.HashAlgorithmNotFoundException;
+import com.ssafy.petandpeople.common.exception.user.NullHashBytesException;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import javax.crypto.*;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 public class PasswordEncryptor {
 
@@ -17,78 +16,32 @@ public class PasswordEncryptor {
     }
 
     public static byte[] getSHA256(String source, String salt) {
-        byte byteData[]=null;
-
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(source.getBytes());
-            md.update(salt.getBytes());
-            byteData= md.digest();
+            return generateHash("SHA-256",source, salt);
         } catch(NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            throw new HashAlgorithmNotFoundException();
         }
-
-        return byteData;
     }
 
-    public static byte[] generateKey(String algorithm,int keySize) throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(algorithm);
-        keyGenerator.init(keySize);
-        SecretKey key = keyGenerator.generateKey();
-
-        return key.getEncoded();
+    public static byte[] generateHash(String algorithm, String source, String salt) throws NoSuchAlgorithmException{
+        MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+        messageDigest.update(source.getBytes());
+        messageDigest.update(salt.getBytes());
+        return messageDigest.digest();
     }
 
-    public static String aesEncrypt(String msg, byte[] key) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        String iv = "AAAAAAAAAAAAAAAA";
-        cipher.init(Cipher.ENCRYPT_MODE,
-                skeySpec,
-                new IvParameterSpec(iv.getBytes()));
+    public static String byteArrayToHex(byte[] hashBytes) {
+        validateHashBytes(hashBytes);
 
-        byte[] encrypted = cipher.doFinal(msg.getBytes());
-
-        return  byteArrayToHex(encrypted);
-    }
-
-    public static String aesDecrypt(String msg,byte[] key ) throws Exception {
-        SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        String iv = "AAAAAAAAAAAAAAAA";
-
-        cipher.init(Cipher.DECRYPT_MODE,
-                skeySpec,
-                new IvParameterSpec(iv.getBytes()));
-
-        byte[] encrypted = hexToByteArray(msg);
-        byte[] original = cipher.doFinal(encrypted);
-
-        return new String(original);
-    }
-
-    public static byte[] hexToByteArray(String hex) {
-
-        if (hex == null || hex.length() == 0) {
-            return null;
-        }
-
-        byte[] ba = new byte[hex.length() / 2];
-        for (int i = 0; i < ba.length; i++) {
-            ba[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
-        }
-
-        return ba;
-    }
-
-    public static String byteArrayToHex(byte[] ba) {
-        if (ba == null || ba.length == 0) {
-            return null;
-        }
-
-        return IntStream.range(0, ba.length)
-                .mapToObj(i -> String.format("%02x", ba[i]))
+        return IntStream.range(0, hashBytes.length)
+                .mapToObj(i -> String.format("%02x", hashBytes[i]))
                 .collect(Collectors.joining());
+    }
+
+    private static void validateHashBytes(byte[] hashBytes) {
+        if (hashBytes == null || hashBytes.length == 0) {
+            throw new NullHashBytesException();
+        }
     }
 
 }
