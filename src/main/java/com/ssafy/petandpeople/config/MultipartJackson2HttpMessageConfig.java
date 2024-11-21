757315d2431c385a5dbfffa6b0dd44ee456f007f
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-11-21 11:28:49 +0900

    Type : feat Subject : config 파일 구현

---
+++ src/main/java/com/ssafy/petandpeople/config/MultipartJackson2HttpMessageConfig.java
@@ -0,0 +1,32 @@
+package com.ssafy.petandpeople.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+import java.lang.reflect.Type;
+
+@Configuration
+public class MultipartJackson2HttpMessageConfig extends AbstractJackson2HttpMessageConverter {
+
+    public MultipartJackson2HttpMessageConfig(ObjectMapper objectMapper) {
+        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    protected boolean canWrite(MediaType mediaType) {
+        return false;
+    }
+
+}
