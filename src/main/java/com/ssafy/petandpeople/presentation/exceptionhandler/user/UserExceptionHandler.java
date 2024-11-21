af3602f89d5f33bebaaabea1791a9e1ed6bec7ca
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-11-21 11:10:59 +0900

    Type : remove Subject : 불필요 클래스 삭제

--- src/main/java/com/ssafy/petandpeople/presentation/exceptionhandler/user/UserExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.petandpeople.presentation.exceptionhandler.user;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class UserExceptionHandler {
-
-}
