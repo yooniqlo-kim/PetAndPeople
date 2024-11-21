57a294f9ebb402493ba8a565b04d8696ad2a66ec
parkkyoungbeom <gyoungbeom98@google.comgit config --global --listgit config --global user.email gyoungbeom98@google.com>
2024-11-21 16:05:09 +0900

    Type : refactor Subject : 로그인 시도 횟수 관련 로직 리팩토링

--- src/main/java/com/ssafy/petandpeople/application/service/user/UserService.java
+++ src/main/java/com/ssafy/petandpeople/application/service/user/UserService.java
@@ -7,6 +7,7 @@ import com.ssafy.petandpeople.application.dto.user.UserDto;
 import com.ssafy.petandpeople.common.exception.user.InvalidSessionException;
 import com.ssafy.petandpeople.common.exception.user.UserNotFoundException;
 import com.ssafy.petandpeople.common.utils.PasswordEncryptor;
+import com.ssafy.petandpeople.domain.user.LoginAttempt;
 import com.ssafy.petandpeople.domain.user.User;
 import com.ssafy.petandpeople.infrastructure.persistence.entity.user.UserEntity;
 import com.ssafy.petandpeople.infrastructure.persistence.entity.user.UserSecurityEntity;
@@ -19,14 +20,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private final LoginAttempt loginAttempt;
     private final UserRepository userRepository;
     private final UserSecurityRepository userSecurityRepository;
-    private final LoginAttemptManager loginAttemptManager;
 
-    public UserService(UserRepository userRepository, UserSecurityRepository userSecurityRepository, LoginAttemptManager loginAttemptManager) {
+    public UserService(LoginAttempt loginAttempt, UserRepository userRepository, UserSecurityRepository userSecurityRepository) {
+        this.loginAttempt = loginAttempt;
         this.userRepository = userRepository;
         this.userSecurityRepository = userSecurityRepository;
-        this.loginAttemptManager = loginAttemptManager;
     }
 
     public Boolean signUp(UserDto userDto) {
@@ -42,23 +43,16 @@ public class UserService {
         return true;
     }
 
-    private User createEncryptedUser(UserDto userDto, String salt) {
-        User user = UserConverter.dtoToDomain(userDto);
-        user.encryptPassword(salt);
-
-        return user;
-    }
-
     public Boolean login(LoginDto loginDto, HttpServletRequest request) {
         String userId = loginDto.getUserId();
 
+        loginAttempt.validateUserLock(userId);
+        loginAttempt.increaseLoginAttempt(userId);
+
         UserEntity userEntity = findLoginUser(userId);
-
-        loginAttemptManager.validateLoginAttempts(userId);
-
         validateLoginPassword(loginDto, userEntity);
 
-        loginAttemptManager.clearLoginAttempts(userId);
+        loginAttempt.resetLoginAttempt(userId);
 
         saveLoginUserInSession(userEntity.getUserKey(), request);
 
@@ -67,6 +61,21 @@ public class UserService {
 
     public UserEntity findLoginUser(String userId) {
         return userRepository.findByUserId(userId).orElseThrow(UserNotFoundException::new);
+    }
+
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        validateSessionExists(session);
+
+        session.invalidate();
+    }
+
+    private User createEncryptedUser(UserDto userDto, String salt) {
+        User user = UserConverter.dtoToDomain(userDto);
+        user.encryptPassword(salt);
+
+        return user;
     }
 
     private void validateLoginPassword(LoginDto loginDto, UserEntity userEntity) {
@@ -97,16 +106,8 @@ public class UserService {
         session.setAttribute("IP_ADDRESS", ipAddress);
     }
 
-    public void logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-
-        validateSessionExists(session);
-
-        session.invalidate();
-    }
-
     private void validateSessionExists(HttpSession session) {
-        if(session == null) {
+        if (session == null) {
             throw new InvalidSessionException();
         }
     }
---
+++ src/test/java/com/ssafy/petandpeople/domain/user/LoginAttemptTest.java
@@ -0,0 +1,52 @@
+package com.ssafy.petandpeople.domain.user;
+
+import com.ssafy.petandpeople.common.exception.user.LoginAttemptExceededException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class LoginAttemptTest {
+
+    private static final String testUserId = "testId";
+
+    private final LoginAttempt loginAttempt;
+
+    @Autowired
+    public LoginAttemptTest(LoginAttempt loginAttempt) {
+        this.loginAttempt = loginAttempt;
+    }
+
+    @AfterEach
+    void setDown() {
+        loginAttempt.resetLoginAttempt(testUserId);
+    }
+
+    @Test
+    @DisplayName("유저의 계정이 잠기지 않음")
+    void validateUserLock_성공() {
+        assertTrue(loginAttempt.validateUserLock(testUserId));
+    }
+
+    @Test
+    @DisplayName("유저의 계정이 잠김")
+    void validateUserLock_에러발생() {
+        for (int count = 1; count <= 5; count++) {
+            loginAttempt.increaseLoginAttempt(testUserId);
+        }
+        assertThrows(LoginAttemptExceededException.class, () -> loginAttempt.validateUserLock(testUserId));
+    }
+
+    @Test
+    @DisplayName("로그인 시도 횟수 증가")
+    void increaseLoginAttempt() {
+        assertEquals(1, loginAttempt.increaseLoginAttempt(testUserId));
+    }
+
+}
