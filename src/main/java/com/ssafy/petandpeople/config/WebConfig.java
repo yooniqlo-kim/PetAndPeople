5b7b34c9bf564bdf469b06578fa0e649bf93bf6f
Ubuntu <yoon733@naver.com>
2024-12-17 17:03:35 +0900

    Type : refactor Subject : 설정파일 수정

--- src/main/java/com/ssafy/petandpeople/config/WebConfig.java
+++ src/main/java/com/ssafy/petandpeople/config/WebConfig.java
@@ -10,9 +10,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 URL 경로에 대해 CORS 설정 적용
-                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500") // 허용할 출처를 명확히 지정
+                .allowedOrigins("https://petandpeople.site")
                 .allowedMethods("GET", "POST", "OPTIONS") // 허용할 HTTP 메서드
                 .allowCredentials(true) // 쿠키 인증 허용
                 .maxAge(3600); // Preflight 요청 캐싱 시간 (1시간)
     }
+
+
 }
+
